888840b13c49e63e41eb195081b41e57f8755cf1
BiederneBachstelze <harder.bardowick@gmail.com>
2024-12-20 13:23:41 +0100

    footer container added bootstrap

--- src/components/FooterContainer.vue
+++ src/components/FooterContainer.vue
@@ -3,7 +3,11 @@
 </script>
 
 <template>
-
+  <footer class="row g-3 mb-4">
+    <div class="col-12 col-12 bg-success text-black text-center p-3">
+      Footer (100%)
+    </div>
+  </footer>
 </template>
 
 <style scoped>
