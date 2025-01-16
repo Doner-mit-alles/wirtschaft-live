f2cc3f9823c4fce3f7a7bfb13dc92edcf0c953cd
Huss15 <hussiosman@gmail.com>
2025-01-16 21:07:43 +0100

    30: add first structure

--- src/components/teamSpace/TeamMember.vue
+++ src/components/teamSpace/TeamMember.vue
@@ -1,22 +1,50 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps({
+  memberName: {
+    type: String,
+    required: true
+  },
+  imagePath: {
+    type: String,
+    required: true
+  }
+})
+</script>
 
 <template>
   <div class="teamMember">
-    <img src="@/assets/images/people/men1.jpg" style="width: 170px; height: 170px;" alt="">
-    <p style="color: white">Hussein</p>
+    <img
+      :src="imagePath"
+      style="
+        width: 150px;
+        height: 160px;
+        border-radius: 40px;
+        box-shadow: 0 4px 4px rgba(0, 0, 0, 0.5);
+      "
+      alt=""
+    />
+    <p style="color: white; margin-top: 10px">{{ memberName }}</p>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .teamMember {
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  border: 1px solid black;
   width: 250px;
-  height: 300px;
-  border-radius: 40px;
+  height: 280px;
   box-shadow: 4px 8px 6px rgba(0, 0, 0, 0.5);
+  border-radius: 40px;
+  border: solid silver 1px;
+  background-color: #ffffff1a;
+  backdrop-filter: blur(50px);
+}
+
+.dark-background {
+  background-color: var(--primary-color);
+  border-radius: 20px;
+  box-shadow: var(--primary-shadow);
 }
 </style>
--- src/components/teamSpace/TeamSpace.vue
+++ src/components/teamSpace/TeamSpace.vue
@@ -1,57 +1,13 @@
 <script setup lang="ts">
 import TeamMember from '@/components/teamSpace/TeamMember.vue'
+import menOne from '@/assets/images/people/men1.jpg'
+import menTwo from '@/assets/images/people/men2.jpg'
+import menThree from '@/assets/images/people/men3.jpg'
+import menFour from '@/assets/images/people/men4.jpg'
 </script>
 
 <template>
-  <!--  <div class="row mb-4">-->
-  <!--    <div class="row justify-content-center">-->
-  <!--      <div class="col-md-6">-->
-  <!--        <div class="gradient-border p-3">-->
-  <!--          <div class="col-md-4 col-12 bg-primary text-white text-center p-0 border-radius-20-left">-->
-  <!--            <img-->
-  <!--              style="width: 300px; height: 300px"-->
-  <!--              src="@/assets/images/BallerLosLoggo.png"-->
-  <!--              alt=""-->
-  <!--            />-->
-  <!--          </div>-->
-  <!--          <div style="background-color: var(&#45;&#45;primary-color)">-->
-  <!--            <h2 style="color: white">Das Team</h2>-->
-  <!--          </div>-->
-  <!--        </div>-->
-  <!--      </div>-->
-  <!--    </div>-->
-  <!--  </div>-->
-  <!--  <div class="row justify-content-center p-3 tempClass">-->
-  <!--    <div class="row mb-4" id="aboutUs">-->
-  <!--      <div class="">-->
-  <!--        <div class="col-md-4 col-12 bg-primary text-white text-center p-0 border-radius-20-left">-->
-  <!--          <img-->
-  <!--            class="object-fit-cover w-100 h-100 border-radius-20-left"-->
-  <!--            :alt="$t('containers.aboutUs.imageAltText')"-->
-  <!--            src="@/assets/images/BallerLosLoggo.png"-->
-  <!--          />-->
-  <!--        </div>-->
-  <!--        <div-->
-  <!--          class="col-md-7 col-12 bg-primary text-white text-left p-4 pl-3 p-md-5 border-radius-20-right"-->
-  <!--        >-->
-  <!--          <h2 class="font-bold">Das Team</h2>-->
-  <!--          <div>-->
-  <!--            <p>-->
-  <!--              At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no-->
-  <!--              sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,-->
-  <!--              consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et-->
-  <!--              dolore magna aliquyam erat, sed diam volup-->
-  <!--            </p>-->
-  <!--          </div>-->
-  <!--        </div>-->
-  <!--      </div>-->
-  <!--    </div>-->
-  <!--    <TeamMember />-->
-  <!--    <TeamMember />-->
-  <!--    <TeamMember />-->
-  <!--  </div>-->
-
-  <div class="row mb-4" id="team" style="border: 1px white solid">
+  <div class="row mb-4 gradient-box" id="team">
     <div class="col-md-12 col-12 text-center p-3">
       <div class="row g-3 m-1">
         <div class="col-md-1 col-12 p-3"></div>
@@ -65,7 +21,7 @@ import TeamMember from '@/components/teamSpace/TeamMember.vue'
         <div class="col-md-6 col-12 text-light p-3 dark-background">
           <h2 style="text-align: left">Das Team</h2>
 
-          <p style="text-align: left; font-size: 18px; margin-top: 30px;">
+          <p style="text-align: left; font-size: 18px; margin-top: 30px">
             At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
             sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
             consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
@@ -74,46 +30,51 @@ import TeamMember from '@/components/teamSpace/TeamMember.vue'
         </div>
         <div class="col-md-1 col-12 p-3"></div>
       </div>
-      <!-- Row mit 3 Spalten -->
       <div class="row g-3 m-1">
-        <div class="col-md-3 col-12 bg-success text-white text-center p-3">Team Foto 1</div>
-        <div class="col-md-3 col-12 d-none d-md-block bg-info text-white text-center p-3">
-          Team Foto 2
+        <div class="col-md-3 col-12 d-none d-md-block text-white text-center p-3">
+          <TeamMember member-name="Harri" :imagePath="menOne" />
         </div>
-        <div class="col-md-3 col-12 d-none d-md-block bg-danger text-white text-center p-3">
-          Team Foto 3
+        <div class="col-md-3 col-12 d-none d-md-block text-white text-center p-3">
+          <TeamMember member-name="Hussein" :imagePath="menTwo" />
         </div>
-        <div class="col-md-3 col-12 d-none d-md-block bg-info text-white text-center p-3">
-          Team Foto 4
+        <div class="col-md-3 col-12 d-none d-md-block text-white text-center p-3">
+          <TeamMember member-name="Felix" :imagePath="menThree" />
+        </div>
+        <div class="col-md-3 col-12 d-none d-md-block text-white text-center p-3">
+          <TeamMember member-name="Cassandra" :imagePath="menFour" />
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .dark-background {
   background-color: var(--primary-color);
   border-radius: 20px;
   box-shadow: var(--primary-shadow);
 }
 
-.gradient-border {
+.gradient-box {
   position: relative;
-  padding: 1px;
-  border-radius: 20px;
-  background: white;
-}
+  box-sizing: border-box;
+  $border: 5px;
+  background: var(--sixth-color);
+  background-clip: padding-box;
+  border: solid $border transparent;
+  border-radius: 1.5em;
 
-.gradient-border::before {
-  content: '';
-  position: absolute;
-  top: -2px;
-  left: -2px;
-  right: -2px;
-  bottom: -2px;
-  background: linear-gradient(to right, #2c86a5 29%, #4bc3b6 64%);
-  z-index: -1;
-  border-radius: inherit;
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    margin: -$border;
+    border-radius: inherit;
+    background: linear-gradient(to right, #2c86a5 29%, #4bc3b6 64%);
+  }
 }
 </style>
