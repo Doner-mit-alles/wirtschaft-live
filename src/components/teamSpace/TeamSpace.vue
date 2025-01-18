378054bb00090e78afe4137a0b4247a2e56540b4
Hussein Osman <hussein.osman@koerber.com>
2025-01-18 19:29:25 +0100

    30: replace single pictures with class photo

--- src/components/teamSpace/TeamSpace.vue
+++ src/components/teamSpace/TeamSpace.vue
@@ -1,26 +1,21 @@
 <script setup lang="ts">
-import TeamMember from '@/components/teamSpace/TeamMember.vue'
-import menOne from '@/assets/images/people/men1.jpg'
-import menTwo from '@/assets/images/people/men2.jpg'
-import menThree from '@/assets/images/people/men3.jpg'
-import menFour from '@/assets/images/people/men4.jpg'
 </script>
 
 <template>
-  <div class="row mb-4 gradient-box" id="team">
+  <div class="row mb-4  border-radius-20" id="team">
     <div class="col-md-12 col-12 text-center p-3">
       <div class="row g-3 m-1">
-        <div class="col-md-1 col-12 p-3"></div>
-        <div class="col-md-4 col-12 p-3">
-          <img
-            class="object-fit-cover w-100 h-150 border-radius-20-left border-radius-20-right"
-            :alt="$t('containers.aboutUs.imageAltText')"
-            src="@/assets/images/BallerLosLoggo.png"
-          />
+        <div class="col-lg-4 col-12">
+          <div class="test h-100 border-radius-20">
+            <img
+              class="object-fit-cover border-radius-20-left border-radius-20-right logo-image"
+              :alt="$t('containers.aboutUs.imageAltText')"
+              src="@/assets/images/BallerLosLoggo.png"
+            />
+          </div>
         </div>
-        <div class="col-md-6 col-12 text-light p-3 dark-background">
+        <div class="col-lg-8 col-12 text-light p-3 gradient-box-color-transition">
           <h2 style="text-align: left">Das Team</h2>
-
           <p style="text-align: left; font-size: 18px; margin-top: 30px">
             At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
             sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
@@ -28,38 +23,57 @@ import menFour from '@/assets/images/people/men4.jpg'
             magna aliquyam erat, sed diam volup
           </p>
         </div>
-        <div class="col-md-1 col-12 p-3"></div>
       </div>
-      <div class="row g-3 m-1">
-        <div class="col-md-3 col-12 d-none d-md-block text-white text-center p-3">
-          <TeamMember member-name="Harri" :imagePath="menOne" />
-        </div>
-        <div class="col-md-3 col-12 d-none d-md-block text-white text-center p-3">
-          <TeamMember member-name="Hussein" :imagePath="menTwo" />
-        </div>
-        <div class="col-md-3 col-12 d-none d-md-block text-white text-center p-3">
-          <TeamMember member-name="Felix" :imagePath="menThree" />
-        </div>
-        <div class="col-md-3 col-12 d-none d-md-block text-white text-center p-3">
-          <TeamMember member-name="Cassandra" :imagePath="menFour" />
-        </div>
+      <div class="row g-5 m-1" style="display: flex; justify-content: center;">
+
+        <img src="@/assets/images/ClassPhoto.jpeg" alt="">
       </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.test {
+  background-color: #d9d9d9;
+}
+
+.logo-image {
+  @media (max-width: 767px) {
+    width: 100%;
+  }
+
+  @media (max-width: 1400px) and (min-width: 992px) {
+    width: 100%;
+  }
+
+  @media (min-width: 768px) and (max-width: 992px) {
+    max-height: 180px;
+  }
+}
+
+.class-photo-image{
+  width: 80%;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+}
+
 .dark-background {
   background-color: var(--primary-color);
   border-radius: 20px;
   box-shadow: var(--primary-shadow);
 }
 
-.gradient-box {
+.gradient-box{
+  //border: solid 1px var(--fifth-color);
+  //background-color: #acacac;
+}
+
+.gradient-box-color-transition {
   position: relative;
   box-sizing: border-box;
-  $border: 5px;
-  background: var(--sixth-color);
+  $border: 2px;
+  background: var(--primary-color);
   background-clip: padding-box;
   border: solid $border transparent;
   border-radius: 1.5em;
