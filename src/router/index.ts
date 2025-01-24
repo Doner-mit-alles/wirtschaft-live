0e4edbb2ca5360c1781a1a5f2330c0e55d726efc
Hussein Osman <hussein.osman@koerber.com>
2025-01-24 23:37:11 +0100

    40: Add title in router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import i18n from '@/i18n'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: () => import('@/views/HomeView.vue'),
-    meta: { title: 'Home' }
+    meta: {  title: 'Baller Los' }
   },
   {
     path: '/impressum-und-datenschutz',
     name: 'Imprint and Privacy Police',
     component: () => import('@/views/ImprintAndPrivacyPoliceView.vue'),
+    meta: { title: 'Imprint and Data Protection' }
   },
   // Catch-all route for undefined paths
   {
