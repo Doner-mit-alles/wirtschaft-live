63be7bb08bb8881d8e8dba5af750c68e77e17ba6
CSS-png <cassandrafischer1@gmail.com>
2025-01-31 11:33:40 +0100

    [49] set privacy police

--- src/components/container/PrivacyPoliceContainer.vue
+++ src/components/container/PrivacyPoliceContainer.vue
@@ -1,35 +1,69 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
   <div class="row mb-4">
     <div class="col-12 bg-primary text-white privacy-police p-4 pl-3 p-md-5 default-shadow">
       <h2 class="break" v-html="$t('container.privacyPolice.headline')"></h2>
 
-      <p>
-        {{ $t('container.privacyPolice.generalInfo') }}
-      </p>
+        <h6 v-html="$t('container.privacyPolice.text1')"/>
+        <p v-html="$t('container.privacyPolice.text2')"/>
+        <p v-html="$t('container.privacyPolice.text3')"/>
+        <p v-html="$t('container.privacyPolice.text4')"/>
+        <p v-html="$t('container.privacyPolice.text5')"/>
+        <p v-html="$t('container.privacyPolice.text6')"/>
+        <p v-html="$t('container.privacyPolice.text7')"/>
+        <p v-html="$t('container.privacyPolice.text8')"/>
+        <p v-html="$t('container.privacyPolice.text9')"/>
+        <p v-html="$t('container.privacyPolice.text10')"/>
+        <p v-html="$t('container.privacyPolice.text11')"/>
+        <p v-html="$t('container.privacyPolice.text12')"/>
 
-      <p>
-        BBS I Lüneburg<br />
-        Spillbrunnenweg 1<br />
-        21337 Lüneburg<br />
-      </p>
+        <h6 v-html="$t('container.privacyPolice.text13')"/>
+        <p v-html="$t('container.privacyPolice.text14')"/>
+        <p v-html="$t('container.privacyPolice.text15')"/>
+        <p v-html="$t('container.privacyPolice.text16')"/>
+        <p v-html="$t('container.privacyPolice.text17')"/>
+        <p v-html="$t('container.privacyPolice.text18')"/>
 
-      <h6>{{ $t('container.privacyPolice.secondHeadline') }}</h6>
-      <p>{{ $t('container.privacyPolice.secondContent1') }}</p>
-      <ul>
-        <li>{{ $t('container.privacyPolice.listElement1') }}</li>
-        <li>{{ $t('container.privacyPolice.listElement2') }}</li>
-        <li>{{ $t('container.privacyPolice.listElement3') }}</li>
-      </ul>
-      <p>
-        {{ $t('container.privacyPolice.secondContent2') }}
-      </p>
+        <h6 v-html="$t('container.privacyPolice.text19')"/>
+        <p v-html="$t('container.privacyPolice.text20')"/>
+        <p v-html="$t('container.privacyPolice.text21')"/>
+        <p v-html="$t('container.privacyPolice.text22')"/>
+        <p v-html="$t('container.privacyPolice.text23')"/>
+        <p v-html="$t('container.privacyPolice.text24')"/>
+        <p v-html="$t('container.privacyPolice.text25')"/>
+        <p v-html="$t('container.privacyPolice.text26')"/>
+        <p v-html="$t('container.privacyPolice.text27')"/>
+        <p v-html="$t('container.privacyPolice.text28')"/>
+        <p v-html="$t('container.privacyPolice.text29')"/>
+        <p v-html="$t('container.privacyPolice.text30')"/>
+        <p v-html="$t('container.privacyPolice.text31')"/>
+        <p v-html="$t('container.privacyPolice.text32')"/>
+        <p v-html="$t('container.privacyPolice.text33')"/>
+        <p v-html="$t('container.privacyPolice.text34')"/>
+        <p v-html="$t('container.privacyPolice.text35')"/>
+        <p v-html="$t('container.privacyPolice.text36')"/>
+        <p v-html="$t('container.privacyPolice.text37')"/>
+        <p v-html="$t('container.privacyPolice.text38')"/>
+        <p v-html="$t('container.privacyPolice.text39')"/>
+        <p v-html="$t('container.privacyPolice.text40')"/>
+        <p v-html="$t('container.privacyPolice.text41')"/>
+        <p v-html="$t('container.privacyPolice.text42')"/>
+        <p v-html="$t('container.privacyPolice.text43')"/>
+        <ul>
+          <li v-html="$t('container.privacyPolice.text44Li')"></li>
+          <li v-html="$t('container.privacyPolice.text45Li')"></li>
+          <li v-html="$t('container.privacyPolice.text46Li')"></li>
+          <li v-html="$t('container.privacyPolice.text47Li')"></li>
+        </ul>
+        <p v-html="$t('container.privacyPolice.text48')"/>
+        <p v-html="$t('container.privacyPolice.text49')"/>
+        <p v-html="$t('container.privacyPolice.text50')"/>
 
-      <h6>{{ $t('container.privacyPolice.thirdHeadline') }}</h6>
-      <p>
-        {{ $t('container.privacyPolice.thirdContent') }}
-      </p>
+        <h6 v-html="$t('container.privacyPolice.text51')"/>
+        <p v-html="$t('container.privacyPolice.text52')"/>
+        <p v-html="$t('container.privacyPolice.text53')"/>
 
       <h6>{{ $t('container.privacyPolice.fourthHeadline') }}</h6>
       <p>
@@ -39,9 +73,9 @@
         >
       </p>
 
-      <p>
-        {{ $t('container.privacyPolice.time') }}
-      </p>
+<!--      <p>-->
+<!--        {{ $t('container.privacyPolice.time') }}-->
+<!--      </p>-->
     </div>
   </div>
 </template>
